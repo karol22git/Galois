8fa3f3d7a1a106243d4880c990850c320ae5187d
karo22git <kamel.poczta676@gmail.com>
2025-11-13 13:20:43 +0100

    cleaning cd

--- AlgebraicOperations.hpp
+++ AlgebraicOperations.hpp
@@ -1,3 +1,4 @@
+#pragma once
 #include "AlgebraicConcepts.hpp"
 
 
@@ -5,5 +6,9 @@ namespace galois {
     template <HasModulo T>
     T Modulo5(T a, T b) {
         return (a+b)%5;
+    }
+    template <HasModulo T, int n>
+    T Modulo(T a, T b) {
+        return (a+b)%n;
     }
 }
